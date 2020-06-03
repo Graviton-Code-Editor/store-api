bc5c265143c5ab36a56e57d8f4e0af66dcbbe509
Marc Espín Sanz <mespinsanz@gmail.com>
2020-06-03 15:20:10 +0200

    :sparkles: Added more graviton version channels

--- models/version.js
+++ models/version.js
@@ -1,9 +1,9 @@
-const pluginsList = require('../data/store.json')
+const storeData = require('../data/store.json')
 const packageJSON = require('../package.json')
 
 exports.apiVersion = () => {
   return {
     apiVersion: packageJSON.version,
-    gravitonVersion: pluginsList.graviton.version,
+    graviton: storeData.graviton,
   }
 }
